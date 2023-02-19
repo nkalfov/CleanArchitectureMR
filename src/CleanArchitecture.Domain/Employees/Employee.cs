1501cc8a5ee2bd139150fd20c0d2348a1d1d63dd
nkalfov <nkalfov@gmail.com>
2023-02-19 23:25:25 +0200

    Removed unused usings

--- src/CleanArchitecture.Domain/Employees/Employee.cs
+++ src/CleanArchitecture.Domain/Employees/Employee.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CleanArchitecture.Domain.Common;
 
 namespace CleanArchitecture.Domain.Employees
