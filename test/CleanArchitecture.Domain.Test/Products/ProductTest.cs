e725bc4b0bead33ac2dfd9cc41335a53dfb18ff0
nkalfov <nkalfov@gmail.com>
2023-02-19 22:27:07 +0200

    Added Unit Test for Domain Entity Product

---
+++ test/CleanArchitecture.Domain.Test/Products/ProductTest.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using CleanArchitecture.Domain.Products;
+
+namespace CleanArchitecture.Domain.Test.Products
+{
+    public class ProductTest
+    {
+        [Theory(DisplayName = "Initializing Products")]
+        [InlineData(1, "Doombringer (Champion Sword)", 5999.99)]
+        [InlineData(1, "Flamebellow (Balrog Blade)", 7459.55)]
+        [InlineData(3, "The Grandfather (Colossus Blade)", 9999.98)]
+        public void InitializingProducts(int id, string name, decimal price)
+        {
+            var actual = new Product
+            {
+                Id = id,
+                Name = name,
+                Price = price
+            };
+
+            Assert.Equal(id, actual.Id);
+            Assert.Equal(name, actual.Name);
+            Assert.Equal(price, actual.Price);
+        }
+
+    }
+}
