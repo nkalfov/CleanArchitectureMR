d77b035ba41f9db6b30f1f8e5cf96530ab06ef38
Nikola Kalfov <nkalfov@gmail.com>
2023-03-03 23:47:48 +0200

    checking yml action on github

--- src/CleanArchitecture.Infrastructure/Models/SaleEventModel.cs
+++ src/CleanArchitecture.Infrastructure/Models/SaleEventModel.cs
@@ -3,24 +3,21 @@ using System.Text.Json.Serialization;
 
 namespace CleanArchitecture.Infrastructure.Models
 {
-	public class SaleEventModel
-	{
-		public SaleEventModel()
-		{
-			// do nothing
-		}
+    public class SaleEventModel
+    {
+        public SaleEventModel() { }
 
-		public SaleEventModel(long productId, int quantity)
-		{
-			ProductId = productId;
-			Quantity = quantity;
-		}
+        public SaleEventModel(long productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
 
-		[JsonPropertyName("productId")]
-		public long ProductId { get; set; }
+        [JsonPropertyName("productId")]
+        public long ProductId { get; set; }
 
-		[JsonPropertyName("quantity")]
-		public int Quantity { get; set; }
-	}
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+    }
 }
 
