dcb43acb7d93ae0b52d4b1b817a4fd4221d0c179
Nikola Kalfov <nkalfov@gmail.com>
2023-04-05 10:56:29 +0300

    changed layout navigation - removed home and privacy and added a url to employees added get employee view

--- src/CleanArchitecture.Presentation/Controllers/EmployeesController.cs
+++ src/CleanArchitecture.Presentation/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 using CleanArchitecture.Application.Employees.Queries.GetEmployeesList;
+using CleanArchitecture.Presentation.Resources;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.Presentation.Controllers
@@ -13,13 +14,22 @@ namespace CleanArchitecture.Presentation.Controllers
             _getEmployeesListQuery = getEmployeesListQuery;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            ViewBag.Title = "Employees";
+            ViewBag.Title = PageTitles.EmployeesIndex;
 
             var employees = await _getEmployeesListQuery.ExecuteAsync();
 
             return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Title = PageTitles.EmployeesCreate;
+
+            return View();
         }
     }
 }
---
+++ src/CleanArchitecture.Presentation/Resources/PageTitles.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CleanArchitecture.Presentation.Resources
+{
+    public static class PageTitles
+    {
+        public const string EmployeesIndex = "Employees";
+        public const string EmployeesCreate = "Create an Employee";
+    }
+}
+
